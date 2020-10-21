b2b5c2a94ce30d5576a3805c06a56c02c5b315c3
Maciej Turlo <maciej.turlo@yougov.com>
2020-10-21 03:06:14 +0200

    grades

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -5,13 +5,61 @@ import net.ruippeixotog.scalascraper.dsl.DSL.Extract._
 import net.ruippeixotog.scalascraper.dsl.DSL.Parse._
 import net.ruippeixotog.scalascraper.browser.JsoupBrowser
 
+import scala.util.matching.Regex
+
 object Main extends IOApp {
 
   private val logger = Logger("main")
   private val browser = JsoupBrowser()
 
-  case class Route(name: String, authorGrade: String, communityGrade: String, style: Option[String]) {
+  case class Route(name: String, authorGrade: Grade, communityGrade: Grade, style: Option[String]) {
     def isSent: Boolean = style.isDefined
+  }
+
+  sealed trait Grade
+  case class FullGrade(number: Int, letter: String, plus: Boolean) extends Grade {
+    override def toString: String = s"$number$letter${if(plus)"+" else ""}"
+  }
+  case class BorderGrade(lower: Grade, upper: Grade) extends Grade {
+    override def toString: String = s"$lower/$upper"
+  }
+  case object UnknownGrade extends Grade
+  object Grade {
+    implicit val ordering: Ordering[Grade] = {
+      Ordering.fromLessThan {
+        case (l: FullGrade, r: FullGrade) => false
+        case _                            => false
+      }
+    }
+    object pattern {
+      val number = "\\d"
+      val letter = "[A-C]"
+      val full: Regex = s"($number)($letter)?(\\+)?".r
+      val border: Regex = s"$full/($number)?($letter)(\\+)?".r
+      println(full)
+      println(border)
+      val unknown: String = "?"
+    }
+    def parse(str: String): Grade = {
+      println(s"===str = '${str}'")
+      str match {
+        case pattern.full(number, letter, plus)                                        =>
+          FullGrade(number.toInt, letter, plus == "+")
+        case pattern.border(number, letter, plus, upperNumber, upperLetter, upperPlus) =>
+          val lowerGrade = FullGrade(number.toInt, letter, plus == "+")
+          val upperGrade = (upperNumber, upperLetter, upperPlus) match {
+            case (null, l, p) => lowerGrade.copy(letter = l, plus = p == "+")
+            case (n, null, p) => lowerGrade.copy(number = n.toInt, plus = p == "+")
+            case (n, l, null) => FullGrade(n.toInt, l, plus = false)
+            case _            => ???
+          }
+          BorderGrade(lowerGrade, upperGrade)
+        case pattern.unknown                                                           =>
+          UnknownGrade
+        case ""                                                                        =>
+          UnknownGrade
+      }
+    }
   }
 
   override def run(args: List[String]): IO[ExitCode] = {
@@ -23,9 +71,10 @@ object Main extends IOApp {
           val children = element.children.toList
           val name = element >> text(".name h4")
           val authorGrade = children(1) >> text("h4")
-          val communityGrade = children(2) >> text("h4")
+          val communityGrade = (children(2) >> text("h4")).split(" ").head
+          println(name)
           val style = element >?> text(".btn-active")
-          Route(name, authorGrade, communityGrade, style)
+          Route(name, Grade.parse(authorGrade), Grade.parse(communityGrade), style)
         }
       }
       sentRoutes = routes.filter(_.isSent)
